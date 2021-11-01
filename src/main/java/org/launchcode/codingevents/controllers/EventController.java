8fcf19f318b1646fb6ed270162ab152f93ee1df2
Kelly Jones <kellymj6@gmail.com>
2021-10-31 20:20:50 -0400

    Completed exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,27 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model) {
-        //List<String> events = new ArrayList<>();
-        if (events.isEmpty()) {
-            model.addAttribute("events", "No events yet!");
-        }
-        else {
-            model.addAttribute("events", events);
-        }
-//        events.add("Code With Pride");
-//        events.add("Strange Loop");
-//        events.add("SpringOne Platform");
-//        model.addAttribute("events", events);
+        events.put("Menteaship", "A fun meetup for connecting with mentors");
+        events.put("Code With Pride", "A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+        model.addAttribute("events", events);
         return "events/index";
     }
 
@@ -40,8 +34,8 @@ public class EventController {
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName) {
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription) {
+        events.put(eventName, eventDescription);
         return "redirect:"; // root path can be left off
     }
 
